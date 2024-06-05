f5b17b6c65e1a17ade6709001f270e2ca7f10250
Zephyr Lin <lzh910954163@gmail.com>
2024-06-04 22:02:14 -0500

    routing

---
+++ src/pages/DashboardLayout.jsx
@@ -0,0 +1,5 @@
+function DashboardLayout() {
+  return <h1>DashboardLayout</h1>;
+}
+
+export default DashboardLayout;
---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+function HomeLayout() {
+  return (
+    <div>
+      <nav>navbar</nav>
+      <Outlet />
+    </div>
+  );
+}
+
+export default HomeLayout;
