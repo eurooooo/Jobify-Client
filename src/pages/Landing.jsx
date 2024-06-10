ee1808daea88431988b58fb99ff8d4d43dccdc08
Zephyr Lin <lzh910954163@gmail.com>
2024-06-10 12:25:15 -0500

    Error Page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,7 @@
 import Wrapper from "../assets/wrappers/LandingPage";
 import main from "../assets/images/main.svg";
 import { Link } from "react-router-dom";
-import Logo from "../assets/components/Logo";
+import Logo from "../components/Logo";
 
 function Landing() {
   return (
